3405fa2aba6f42529c1b10d1f10ec4b7760cf186
Helen2813 <krikun@uchi.ru>
2021-11-14 20:43:05 +0300

    init redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,23 @@ import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
+import { Provider } from "react-redux";
+import { store } from './store';
 
 const App = () => {
   return (
-    <Router>
-        <>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={ <Landing /> } />
-                {/*<section className="container">*/}
-                        <Route exact path="/register" element={<Register />} />
-                        <Route exact path="/login" element={<Login />} />
-                {/*</section>*/}
-            </Routes>
-        </>
-    </Router>
+      <Provider store={store}>
+          <Router>
+              <>
+                  <Navbar />
+                  <Routes>
+                      <Route exact path="/" element={ <Landing /> } />
+                      <Route exact path="/register" element={<Register />} />
+                      <Route exact path="/login" element={<Login />} />
+                  </Routes>
+              </>
+          </Router>
+      </Provider>
   );
 }
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,45 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export const Login = () => {
+    const [ formData, setFormData ] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('login')
+    }
     return (
-        <section className="container">Login</section>
+        <section className="container">
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"> Sign into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email"/>
+                    <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email</small
+                    >
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        name="password"
+                        minLength="6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login"/>
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </section>
     )
 }
